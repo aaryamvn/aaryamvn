9f68fde3a8703046ab6825b1d676ce5f12fb9b48
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-12-01 01:37:31 +0530

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  purge: [],
-  darkMode: 'class', // or 'media' or 'class'
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   mode: 'jit',
   theme: {
-    extend: {},
+    colors: {
+      accent: "#5176FF",
+      white: "#FFFFFF",
+      white1: "#DEDEDE",
+      white2: "#ECECEC",
+      black: "#070A0E",
+      black1: "#17202D",
+      black2: "#26364B",
+    }
   },
   variants: {
     extend: {},
