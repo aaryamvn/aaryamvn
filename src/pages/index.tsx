4bd90634fc2f3a8e95dd4c3f44eb115a443b3336
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-12-01 01:55:48 +0530

    Add custom background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,13 @@ import { useContext } from "react";
 import { ThemeContext } from "../modules/ThemeProvider";
 
 export default function Home() {
-  const {} = useContext(ThemeContext);
+  const { colorMode } = useContext(ThemeContext);
 
-  return <div className="w-screen h-screen"></div>;
+  return (
+    <div
+      className={`w-screen h-screen ${
+        colorMode === "light" ? "bg__light" : "bg__dark"
+      }`}
+    ></div>
+  );
 }
