91341e13d8b3225e9cecb0cd822dc395a2eccb0e
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-12-01 09:42:59 +0530

    Improve responsiveness + spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,9 @@ export default function Home() {
         colorMode === "light" ? "bg__light" : "bg__dark"
       }`}
     >
-      <div className="flex flex-col gap-7 container mx-auto w-full md:max-w-[80%] lg:max-w-[70%] xl:max-w-[40rem]">
+      <div className="flex flex-col container mx-auto w-[90%] md:max-w-[60%] lg:max-w-[50%] xl:max-w-[40rem]">
         {/* Socials */}
-        <div className="flex gap-4">
+        <div className="flex gap-2 md:gap-4 mb-[1.5rem]">
           <Social
             Icon={Github}
             href="https://github.com/fullstackslayer"
@@ -37,12 +37,12 @@ export default function Home() {
         </div>
 
         {/* Heading */}
-        <h1 className="text-black2 dark:text-white2 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font__poppins font-extrabold text-left">
+        <h1 className="text-black2 dark:text-white2 text-3xl sm:text-4xl md:text-5xl font__poppins font-extrabold text-left !leading-[1.2] mb-[1.3rem]">
           Hey, I'm <span className="text-accent">Aaryaman</span>
         </h1>
 
         {/* Body */}
-        <p className="text-black2 dark:text-white2 sm:text-lg md:text-[0.5rem] lg:text-xl xl:text-2xl font-medium text-left tracking-wider !leading-[1.5]">
+        <p className="text-black2 dark:text-white2 text-md sm:text-lg lg:text-xl font-medium text-left tracking-wider !leading-[1.5] mb-[1.7rem]">
           I’m a middle-schooler aspiring to build products that{" "}
           <strong>make an impact</strong>. I design and implement elegant
           software with the help of modern tools.
@@ -66,7 +66,7 @@ const Social = ({
 }) => {
   return (
     <a href={href} title={title} target="_blank">
-      <Icon className="fill-[#070A0E] dark:fill-[#FFFFFF] hover:opacity-[0.6] duration-100 ease-in h-[1.7rem] w-[1.7rem]" />
+      <Icon className="fill-[#070A0E] dark:fill-[#FFFFFF] hover:opacity-[0.6] duration-100 ease-in h-[1.4rem] w-[1.4rem] sm:h-[1.5rem] sm:w-[1.5rem] md:h-[1.7rem] md:w-[1.7rem]" />
     </a>
   );
 };
