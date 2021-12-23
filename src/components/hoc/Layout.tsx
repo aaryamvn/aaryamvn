fe3dc449c681a7c15053cd27a2f49a5f5e913d94
Aaryaman Maheshwari <67153585+fullstackslayer@users.noreply.github.com>
2021-12-23 11:06:11 +0530

    refactor elements in nav

--- src/components/hoc/Layout.tsx
+++ src/components/hoc/Layout.tsx
@@ -5,9 +5,9 @@ import { Navbar } from "../ui/Navbar";
 export const Layout: React.FC<{ pageTitle: string }> = ({ children }) => {
   return (
     <div className="w-screen h-screen">
-      <Colorbar height="h-[8px]" />
+      <Colorbar height="h-[6px]" />
       <Navbar height="h-[5.4rem]" />
-      <div className="h-[calc(100vh-5.4rem-10px)] flex justify-center">
+      <div className="h-[calc(100vh-5.4rem-6px)] flex justify-center">
         <div className="mt-7 mx-auto px-10 lg:px-0 lg:w-[40rem]">
           {children}
         </div>
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -5,11 +5,11 @@ import ReactSound from "react-sound";
 export const Navbar: React.FC<{ height: string }> = ({ height }) => {
   return (
     <div
-      className={`w-screen flex items-center justify-between p-4 ${height} border-b border-black2`}
+      className={`w-screen flex items-center justify-between p-4 ${height} border-b border-black1`}
     >
       <div className="flex items-center gap-7">
         <Logo />
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-3">
           <NavElement title="about me" path="/about" />
           <NavElement title="technologies" path="/technologies" />
           <NavElement title="projects" path="/projects" />
@@ -40,7 +40,12 @@ const NavElement: React.FC<{ title: string; path: string }> = ({
   return (
     <>
       <div
-        className={`${active && ""} cursor-pointer`}
+        className={`${
+          active && "bg-white1 dark:bg-black1 py-2 rounded-full font-bold"
+        } 
+        cursor-pointer
+        px-[1rem] -ml-1 mr-1
+        `}
         onClick={async () => {
           setPlayStatus("PLAYING");
           await new Promise((resolve) => setTimeout(resolve, 300));
@@ -57,7 +62,11 @@ const NavElement: React.FC<{ title: string; path: string }> = ({
 const Logo = () => {
   return (
     <a href="/">
-      <img src="/logo.svg" className="" alt="Aaryaman Maheshwari" />
+      <img
+        src="/logo.svg"
+        className="h-[2.7rem] w-[2.7rem]"
+        alt="Aaryaman Maheshwari"
+      />
     </a>
   );
 };
